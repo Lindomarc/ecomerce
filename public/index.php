7f73fc149299f1e93bb7ac583d5abcf67a8a52e8
lindomarc <lindomarc@outlook.com>
2021-06-05 08:00:28 -0300

    fix: recover password

--- public/index.php
+++ public/index.php
@@ -16,67 +16,60 @@
 	const CACHE = TMP . 'cache' . DIRECTORY_SEPARATOR;
 	const VIEW_CACHE = CACHE . 'views' . DIRECTORY_SEPARATOR;
 	
+	const REPLAY_EMAIL = 'no-replay@exemple.com';
+	const REPLAY_NAME = 'no-replay';
+	const FROM_EMAIL = 'exemple2@exemple.com';
+	const FROM_NAME = 'exemple2';
+	
+	define('HTTP_HOST', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']);
+	
 	require_once VENDOR . 'autoload.php';
 	
 	$app = new Slim();
-	$app->log->setEnabled(true);
-	$app->log->setLevel(\Slim\Log::DEBUG);
+	$app->config(array(
+		'log.enable' => true,
+		'debug' => true
+	));
 	
 	$app->get('/', function () {
 		$page = new Page();
 		$page->setTpl('index');
 	});
 	
-	$user = new User();
 	
-	$app->group('/admin', function () use ($app) {
-
-		if (User::verifyLogin()) {
-			header('Location: /admin/login');
-			exit;
-		}
-		/* DASHBOARD */
-		$app->get('/', function () {			
-			$page = new AdminPage();
-			$page->setTpl('index');
-		});
-		
-		
-		/* USERS */
-		$app->group('/users', function () use ($app) {
-			
-			
-			$app->get('/', function () {
-				(new UsersPage)->index();
-			});
-			
-			$app->get('/create', function () {
-				(new UsersPage)->create();
-			});		
-			
-			$app->post('/create', function () {
-				(new UsersPage)->create();
-			});
-			
-			$app->get('/:iduser/delete', function ($iduser) {
-				(new UsersPage)->delete($iduser);
-			});
-			
-			$app->get('/:iduser', function ($iduser) {
-				(new UsersPage)->edit($iduser);
-			});	
-			
-			$app->post('/:iduser', function ($iduser) {
-				(new UsersPage)->edit($iduser);
-			});
-			
-		});
-		
-		
+	/* DASHBOARD */
+	$app->get('/admin', function () {
+		$page = new AdminPage();
+		$page->setTpl('index');
+	});
+	
+	/* USERS */
+	$app->get('/admin/users', function () {
+		(new UsersPage)->index();
+	});
+	
+	$app->get('/admin/users/create', function () {
+		(new UsersPage)->create();
+	});
+	
+	$app->post('/admin/users/create', function () {
+		(new UsersPage)->create();
+	});
+	
+	$app->get('/admin/users/:iduser/delete', function ($iduser) {
+		(new UsersPage)->delete($iduser);
+	});
+	
+	$app->get('/admin/users/:iduser', function ($iduser) {
+		(new UsersPage)->edit($iduser);
+	});
+	
+	$app->post('/admin/users/:iduser', function ($iduser) {
+		(new UsersPage)->edit($iduser);
 	});
 	
 	
-	$app->get('/admin/login', function () {
+	$app->get('/auth/login', function () {
 		$page = new AdminPage([
 			"header" => false,
 			"footer" => false
@@ -84,16 +77,84 @@
 		$page->setTpl("login");
 	});
 	
-	$app->post('/admin/login', function () {
+	$app->post('/auth/login', function () {
 		User::login($_POST['deslogin'], $_POST['despassword']);
 		header('Location: /admin');
 		exit;
 	});
 	
-	$app->get('/admin/logout', function () {
+	
+	$app->get('/auth/logout', function () {
 		User::logout();
-		header('Location: /admin/login');
+		header('Location: /auth/login');
 		exit;
+	});
+	
+	$app->get('/auth/forgot', function () {
+		$page = new AdminPage([
+			"header" => false,
+			"footer" => false
+		]);
+		$page->setTpl("forgot");
+	});
+	
+	$app->post('/auth/forgot', function () use ($app) {
+		if (User::getForgot()) {
+			header("Location: /auth/forgot/send");
+		} else {
+			$app->flash('error', 'Unable to retrieve password.');
+			header("Location: /auth/forgot");
+		}
+		exit();		
+	});
+	
+	$app->get('/auth/forgot/send', function () {		
+		$page = new AdminPage([
+			"header" => false,
+			"footer" => false
+		]);
+		$page->setTpl("forgot-send");
+		exit();
+	});
+	
+	$app->get('/forgot/reset', function () use ($app) {
+		$user = User::validForgotDecrypt($_GET['code']);
+		if (!$user) {
+			$app->flash('error', 'Not Unable to retrieve password');
+		}
+		
+		$page = new AdminPage([
+			"header" => false,
+			"footer" => false
+		]);
+		
+		$page->setTpl("forgot-reset", [
+			'name' => $user['desperson'],
+			'code' => $_GET['code']
+		]);
+	});
+	
+	$app->post('/auth/forgot/reset', function () use ($app) {
+		$forgot = User::validForgotDecrypt($_POST['code']);
+		if (!$forgot) {
+			$app->flash('error', 'Not Unable to retrieve password');
+		}
+		
+		User::setForgotUsed($forgot['idrecovery']);
+		
+		$user = new User();
+		
+		$user->get((int)($forgot['iduser']));
+		
+		$user->setPassword($_POST['password']);
+		
+		$page = new AdminPage([
+			"header" => false,
+			"footer" => false
+		]);
+		
+		$page->setTpl("forgot-reset-success");
+		
 	});
 	
 	$app->run();
